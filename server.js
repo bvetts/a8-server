67b9de10cdd2b407ba2c95f2aba1f547c74da144
Brianna Vetter <bri.vetter@gmail.com>
2022-04-11 14:21:42 -0400

    a9 MONGO sp22

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const app = express();
 app.use(cors());
 //mongodb+srv://webdevMongo:<password>@cluster0.ev2yx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-const CONNECTION_STRING = 'mongodb+srv://webdevMongo:webdevMongo@cluster0.ev2yx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_STRING = 'mongodb+srv://webdevMongo:webdevMongo@cluster0.ev2yx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
  //|| 'mongodb://localhost:27017/webdev'
+//mongoose.connect(CONNECTION_STRING);
+
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
 
 //mongoose.connect('mongodb://localhost:27017/webdev');
